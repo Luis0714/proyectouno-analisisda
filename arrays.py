ac7377f7044595150ae3f08306e4895480354f4f
luisEmilioG1 <luis.1702011450@ucaldas.edu.co>
2023-11-24 19:26:30 -0500

    taller3 parte 1

--- arrays.py
+++ arrays.py
@@ -16,7 +16,7 @@ class Arrays:
         array_channels = []
         self.combos = []
         self.elements = []
-        EstadoCanalF, EstadoEstadoF, EstadoCanalP, EstadoEstadoP = [], [], [], []
+        self.EstadoCanalF, self.EstadoEstadoF, self.EstadoCanalP, self.EstadoEstadoP, self.initial_table_for_taller3 = [], [], [], [], []
 
     def fill_array(self, element, *x: list) -> np.ndarray:
         lengths = [len(i) for i in x]
@@ -29,14 +29,16 @@ class Arrays:
         self.elements = list(set(self.array_channels.flatten()))
         self.combos = self._generate_combos()
         self.element = element
-        self.fill_EstadoCanalF()
-        self.fill_EstadoEstadoF()
-        self.fill_EstadoCanalP()
-        self.fill_EstadoEstadoP()
+        # self.fill_EstadoCanalF()
+        # self.fill_EstadoEstadoF()
+        # self.fill_EstadoCanalP()
+        # self.fill_EstadoEstadoP()
+        self.fill_initial_table_for_taller3()
         return self.array_channels
 
     # for EstadoCanalF 
     def probability_next_element_in_channel_by_system(self, system: list, channel: int, element) -> float:
+        
         if len(system) != len(self.array_channels):
             raise Exception("El system no tiene está completo")
 
@@ -45,8 +47,7 @@ class Arrays:
 
         if not channel in range(len(self.array_channels)):
             raise Exception("channel no válido")
-
-
+        
         index_equal_system = []
         for i in range(len(self.array_channels[0]) - 1):
             system_i = [channel[i] for channel in self.array_channels]
@@ -54,10 +55,6 @@ class Arrays:
                 index_equal_system.append(i)
         ok_next_system = 0
         for i in index_equal_system:
-            
-            if i + 1 >= len(self.array_channels[0]):  # last element
-                continue
-            
             next_system = self.array_channels[channel][i + 1]
             
             if next_system == element:
@@ -65,9 +62,10 @@ class Arrays:
         if len(index_equal_system) == 0:
             return 0
         
-        return ok_next_system / len(index_equal_system)
-
+        return ok_next_system / len(index_equal_system) 
+    
     # for EstadoEstadoF
+    #! for taller 3
     def probability_next_system_by_system(self, system: list, next_system: list) -> float:
         if len(system) != len(self.array_channels) or len(next_system) != len(
             self.array_channels
@@ -77,17 +75,13 @@ class Arrays:
         # TODO: check if next_system an system is in self.array_channels
 
         index_equal_system = []
-        for i in range(len(self.array_channels[0])):
+        for i in range(len(self.array_channels[0]) - 1):
             system_i = [channel[i] for channel in self.array_channels]
             if system == system_i:
                 index_equal_system.append(i)
 
         ok_next_system = 0
         for i in index_equal_system:
-            
-            if i + 1 >= len(self.array_channels[0]):  # last element
-                continue
-            
             real_next_system = [channel[i + 1] for channel in self.array_channels]
             
             if real_next_system == next_system:
@@ -105,15 +99,12 @@ class Arrays:
         # TODO: check if next_system and system is in self.array_channels
 
         index_equal_system = []
-        for i in range(len(self.array_channels[0])):
+        for i in range(len(self.array_channels[0]) - 1):
             if self.array_channels[origin_channel][i] == element:
                 index_equal_system.append(i)
 
         ok_next_system = 0
         for i in index_equal_system:
-            if i + 1 >= len(self.array_channels[0]):  # last element
-                continue
-            
             real_next_system = [channel[i + 1] for channel in self.array_channels]
             
             if real_next_system == next_system:
@@ -131,15 +122,12 @@ class Arrays:
 
         index_equal_system = []
         num_inputs = len(self.array_channels[0])
-        for i in range(num_inputs):
+        for i in range(num_inputs - 1):
             if self.array_channels[origin_channel][i] == origin_element:
                 index_equal_system.append(i)
 
         ok_next_system = 0
         for i in index_equal_system:
-            if i + 1 >= len(self.array_channels[0]):  # last element
-                continue
-            
             real_next_element = self.array_channels[next_channel][i + 1]
             
             if real_next_element == next_element:
@@ -164,7 +152,7 @@ class Arrays:
         new_combinations = []
         for combination in smaller_combinations:
             for e in self.elements:
-                new_combinations.append(combination + [e]) 
+                new_combinations.append([e] + combination) 
 
         return new_combinations
         
@@ -175,43 +163,45 @@ class Arrays:
                 return letra
             else:
                 return "Número fuera de rango"
-
+    
     def fill_EstadoCanalF(self):
-        countChannels = len(self.array_channels)
-        channelsString = ['Canal '+self.obtener_letra_por_numero(i+1)+"\n"+str(self.element) for i in range(countChannels)]
-        systemString = [self.obtener_letra_por_numero(i+1) for i in range(countChannels)]
-        headers = systemString + channelsString
+        count_channels = len(self.array_channels)
+        channels_string = ['Canal '+self.obtener_letra_por_numero(i+1)+"\n"+str(self.element) for i in range(count_channels)]
+        systemString = [self.obtener_letra_por_numero(i+1) for i in range(count_channels)]
+        headers = systemString + channels_string
         systems = self.combos
-        channels = [i for i in range(countChannels)]
+        channels = [i for i in range(count_channels)]
         response = []
         lista = []
-        lista.append(headers)
+        # lista.append(headers)
         for system in systems:
-            probability_list = [i for i in system]
+            
+            # probability_list = [i for i in system]
+            probability_list = []
             system_list = [i for i in system]
             for channel in channels:
                 probability = self.probability_next_element_in_channel_by_system(
                     system=system_list, channel=channel, element=self.element
                 )
-                porcentage = str(round((probability * 100), 2)) + "%"
+                porcentage = probability
                 probability_list.append(porcentage)
             lista.append(probability_list)
             probability_list = []
         self.EstadoCanalF = lista
 
     def fill_EstadoEstadoF(self):
-        countChannels = len(self.array_channels)
-        channelsString = [''.join(map(str, i)) for i in self.combos]
-        comboString = [self.obtener_letra_por_numero(i+1) for i in range(countChannels)]
-        headers = comboString + channelsString
+        count_channels = len(self.array_channels)
+        channels_string = [''.join(map(str, i)) for i in self.combos]
+        combo_string = [self.obtener_letra_por_numero(i+1) for i in range(count_channels)]
+        headers = combo_string + channels_string
         combos = self.combos
-        channels = [i for i in range(countChannels)]
+        channels = [i for i in range(count_channels)]
         lista = []
         lista.append(headers)
         for combo in combos:
             probability_list = copy.copy(combo)
-            for next_combo in combos:
-                probability = self.probability_next_system_by_system(system=combo, next_system=next_combo)
+            for next_state in combos:
+                probability = self.probability_next_system_by_system(system=combo, next_system=next_state)
                 porcentage = str(round((probability * 100), 2)) + "%"
                 probability_list.append(porcentage)
             lista.append(probability_list)
@@ -219,37 +209,117 @@ class Arrays:
         self.EstadoEstadoF = lista
 
     def fill_EstadoCanalP(self):
-        countChannels = len(self.array_channels)
-        channelsString = [''.join(map(str, i)) for i in self.combos]
-        headers = ["-"] + channelsString
+        count_channels = len(self.array_channels)
+        channels_string = [''.join(map(str, i)) for i in self.combos]
+        headers = ["-"] + channels_string
         combos = self.combos
-        channels = [i for i in range(countChannels)]
+        channels = [i for i in range(count_channels)]
         lista = []
         lista.append(headers)
-        for i in range(countChannels):
+        for i in range(count_channels):
             probability_list = ['Canal '+self.obtener_letra_por_numero(i+1)+" ("+str(self.element)+")"]
-            for next_combo in combos:
-                probability = self.probability_next_system_by_elemetn_channel(origin_channel = i, element = self.element, next_system=next_combo)
+            for next_state in combos:
+                probability = self.probability_next_system_by_elemetn_channel(origin_channel = i, element = self.element, next_system=next_state)
                 porcentage = str(round((probability * 100), 2)) + "%"
                 probability_list.append(porcentage)
             lista.append(probability_list)
             probability_list = []
         self.EstadoCanalP = lista
 
-    def fill_EstadoEstadoP(self):
-        countChannels = len(self.array_channels)
-        channelsString = ['Canal '+self.obtener_letra_por_numero(i+1)+"\n"+str(self.element) for i in range(countChannels)]
-        headers = ["-"] + channelsString
+    def fill_EstadoEstadoP(self): 
+        count_channels = len(self.array_channels)
+        channels_string = ['Canal '+self.obtener_letra_por_numero(i+1)+"\n"+str(self.element) for i in range(count_channels)]
+        headers = ["-"] + channels_string
         combos = self.combos
-        channels = [i for i in range(countChannels)]
+        channels = [i for i in range(count_channels)]
         lista = []
         lista.append(headers)
-        for i in range(countChannels):
+        for i in range(count_channels):
             probability_list = ['Canal '+self.obtener_letra_por_numero(i+1)+" ("+str(self.element)+")"]
-            for j in range(countChannels):
+            for j in range(count_channels):
                 probability = self.probability_next_element_in_channel_by_element_channel(origin_channel=i, origin_element=self.element, next_channel=j, next_element=self.element)
                 porcentage = str(round((probability * 100), 2)) + "%"
                 probability_list.append(porcentage)
             lista.append(probability_list)
             probability_list = []
         self.EstadoEstadoP = lista
+        
+    #! for taller 3
+    def fill_initial_table_for_taller3(self):
+        result = []
+        for combo in self.combos:
+            probability_row = []
+            for next_state in self.combos:
+                probability = self.probability_next_system_by_system(system=combo, next_system=next_state)
+                probability_row.append(probability)
+            result.append(probability_row)
+            probability_row = []
+        self.initial_table_for_taller3 = np.array(result)
+        
+    def marginalize_next_systems(self, initial_state:tuple[int], next_state:tuple[int]):
+        table = self.initial_table_for_taller3.copy()
+        quit_channels_col = [i for i in range(len(next_state)) if next_state[i] is None]
+
+        # next_state obtenemos los combos resultantes de quitar los canales
+        new_combos_col = []
+        for combo in self.combos:
+            new_combo = []
+            for i in range(len(combo)):
+                if i not in quit_channels_col:
+                    new_combo.append(combo[i])
+            new_combos_col.append(new_combo)                
+
+        # next_state obtenemos los índices de los combos repetidos        
+        indice_elemento = {}
+        for indice, elemento in enumerate(new_combos_col):
+            if str(elemento) in indice_elemento:
+                indice_elemento[str(elemento)].append(indice)
+            else:
+                indice_elemento[str(elemento)] = [indice]
+        
+        # next_state sumamos las probabilidades de los combos repetidos
+        new_table = None
+        for key in indice_elemento:
+            acc = np.zeros(len(table[:,0]))
+            for indice in indice_elemento[key]:
+                acc = acc + table[:,indice]
+                
+            if new_table is None:
+                new_table = acc
+            else:
+                new_table = np.c_[new_table, acc]        
+                    
+        #! curret_state
+        quit_channels_row = [i for i in range(len(initial_state)) if initial_state[i] is None]
+        
+        # next_state obtenemos los combos resultantes de quitar los canales
+        new_combos_row = []
+        for combo in self.combos:
+            new_combo = []
+            for i in range(len(combo)):
+                if i not in quit_channels_row:
+                    new_combo.append(combo[i])
+            new_combos_row.append(new_combo) 
+            
+        # next_state obtenemos los índices de los combos repetidos 
+        indice_elemento_row = {}
+        for indice, elemento in enumerate(new_combos_row):
+            if str(elemento) in indice_elemento_row:
+                indice_elemento_row[str(elemento)].append(indice)
+            else:
+                indice_elemento_row[str(elemento)] = [indice]
+        
+        # next_state sumamos las probabilidades de los combos repetidos y dividimos entre 2
+        # ! use la fórmula (depronto recusrión)
+        result_table = None
+        for key in indice_elemento_row:
+            acc = np.zeros(len(new_table[0]))
+            for indice in indice_elemento_row[key]:
+                acc = (acc + new_table[indice, :]) /2
+                
+            if result_table is None:
+                result_table = acc   
+            else:
+                result_table = np.vstack((result_table, acc))            
+                
+        return new_table, result_table
